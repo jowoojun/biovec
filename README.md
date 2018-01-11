57a36a0135a1db972c84c0046dca7c91a1c037bf
ChanggunLee <cd8114@gmail.com>
2018-01-11 20:16:03 +0900

    update README.md

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # 2017Bio2Vec
 
 If you don't have Uniprot Database, you can download from the below link.
+=============
  -http://www.uniprot.org/downloads
+
+ have to install biopython
+ ======================
+ pip install biopython
--- bio2vec.py
+++ bio2vec.py
@@ -1,20 +1,20 @@
 from Bio import SwissProt
 
-handle = open("uniprot_sprot.dat") 
+handle = open("uniprot_sprot.dat")
 
 #descriptions = [record.description for record in SwissProt.parse(handle)]
 
 def build_dataset(handle):
-	sequences = list()
-	for record in SwissProt.parse(handle):
-		sequences.append(record.sequence[0:])
-		sequences.append(record.sequence[1:])
-		sequences.append(record.sequence[2:])
-
-	return sequences
+    sequences = list()
+    for record in SwissProt.parse(handle):
+        sequences.append(record.sequence[0:])
+        sequences.append(record.sequence[1:])
+        sequences.append(record.sequence[2:])
+    return sequences
 
 sequences = build_dataset(handle)
 
-print sequences
+
+# print sequences
 
 
