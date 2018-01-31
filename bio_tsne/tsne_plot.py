3caec4e173dee7fa02d0655deae2b82d57f1ec55
jowoojun <jowoojun@naver.com>
2018-01-31 14:11:37 +0900

    Added TSNE data file and fix indentation of tans_plot.py

--- bio_tsne/tsne_plot.py
+++ bio_tsne/tsne_plot.py
@@ -12,29 +12,29 @@ import pickle
 import os
 
 class BioTsne:
-	def __init__(self):
-		print 'TSNE is running..'
+    def __init__(self):
+        print 'TSNE is running..'
 
-        # making tsne
-	def make_tsne(self, model):
+    # making tsne
+    def make_tsne(self, model):
+        if not os.path.isfile("./bio_tsne/tsne.p"):
+            # make tsne
+            X = model[model.wv.vocab]
+            tsne = TSNE(n_components=2)
+            X_tsne = tsne.fit_transform(X)
 
-            if not os.path.isfile("./bio_tsne/tsne.p"):
-                # make tsne
-                X = model[model.wv.vocab]
-                tsne = TSNE(n_components=2)
-                X_tsne = tsne.fit_transform(X)
+            # save X_tsne
+            f = open("./bio_tsne/tsne.p","wb")
+            pickle.dump(X_tsne , f)
 
-                # save X_tsne
-                f = open("./bio_tsne/tsne.p","wb")
-                pickle.dump(X_tsne , f)
-                f.close()
+            f.close()
 
-        def visualization(self):
-                # load X_tsne data
-                f = open( "./bio_tsne/tsne.p" , "rb") 
-                X_tsne = pickle.load(f)
+    def visualization(self):
+        # load X_tsne data
+        f = open( "./bio_tsne/tsne.p" , "rb")
+        X_tsne = pickle.load(f)
 
-                plt.scatter(X_tsne[:,0], X_tsne[:, 1])
-		plt.show()
+        plt.scatter(X_tsne[:,0], X_tsne[:, 1])
+        plt.show()
 
-                f.close()
+        f.close()
