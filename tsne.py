357aebd4c91b3fbcbe1fa4d3a682e67f4d776ad1
ChanggunLee <cd8114@gmail.com>
2018-02-01 19:59:29 +0900

    add tsne_3gram

--- tsne.py
+++ tsne.py
@@ -1,20 +1,20 @@
-import bio_tsne
+import bio_tsne.tsne_3gram as bt
 import biovec
 
-model_fname = "./trained_models/2017trained_model_protein"
-model_3gram = "./trained_models/2017trained_model_3gram"
+#model_fname = "./trained_models/2017trained_model_protein"
+model_3gram = "./trained_models/ngram_model"
 
-model_protein = biovec.models.load_protvec(model_fname)
+#model_protein = biovec.models.load_protvec(model_fname)
 model_3gram = biovec.models.load_protvec(model_3gram)
 
 print "Loading protvec"
 print "... OK\n"
 
-tsne = bio_tsne.BioTsne()
+tsne = bt.BioTsne()
 
 print "Making tsne"
-tsne_protein = tsne.make_tsne(model_protein , model_3gram)
+tsne_protein = tsne.make_tsne(model_3gram )
 print "... OK\n"
 
 print "Visualization"
-tsne.visualization(tsne_protein, tsne_3gram)
+tsne.visualization()
