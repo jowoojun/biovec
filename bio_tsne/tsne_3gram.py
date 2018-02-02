3a6e62c7cc77b4ccf4955de98a2905770fe21789
ChanggunLee <cd8114@gmail.com>
2018-02-02 16:11:33 +0900

    bugfix tsne_3gram

--- bio_tsne/tsne_3gram.py
+++ bio_tsne/tsne_3gram.py
@@ -16,31 +16,31 @@ class BioTsne:
 
     # making tsne
     def make_tsne(self, model):
-        if not os.path.isfile("./bio_tsne/tsne.p"):
+        if not os.path.isfile("./trained_models/ngram_2D_vector"):
             # make tsne
             X = model[model.wv.vocab]
             tsne = TSNE(n_components=2)
             X_tsne = tsne.fit_transform(X)
 
             # save X_tsne
-            f = open("./bio_tsne/tsne.p","wb")
+            f = open("./trained_models/ngram_2D_vector","wb")
             pickle.dump(X_tsne , f)
 
             f.close()
 
     def visualization(self):
         # load X_tsne data
-        f = open( "./bio_tsne/tsne.p" , "rb")
+        f = open( "./trained_models/ngram_2D_vector" , "rb")
         X_tsne = pickle.load(f)
         
         #set marker size
         marker_size=10
         
         #set scatter 
-        plt.scatter(X_tsne[:,0], X_tsne[:, 1],marker_szie , """X_tsne[:,2]""")
+        plt.scatter(X_tsne[:,0], X_tsne[:, 1], marker_size )#,"""X_tsne[:,2]""")
         
         #set color bar 
-        cbar=plt.colorbar()
+        #cbar=plt.colorbar()
         
         #set label
         plt.title("Mass")
