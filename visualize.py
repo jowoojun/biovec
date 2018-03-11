95610680a79ef3c9e3c7eadc6db9e0e4f2b57303
ChanggunLee <cd8114@gmail.com>
2018-03-11 22:31:51 +0900

    change density visualize

--- visualize.py
+++ visualize.py
@@ -5,22 +5,23 @@ import pickle
 import os
 import numpy as np
 import biovisual.bio_visual as bv
+import word2vec 
 print "PS(protrin space) , BSVM(binay svm) , DM(density map)"
 str = raw_input()
 
 # make protein 100D-vec to 2D-vec
 def protein_tsne(dataset_2D , dataset_vec):
-    tsne = tp.BioTsne()
+    tsne = t3.BioTsne()
     if not os.path.isfile(dataset_2D):
-        dataset_vectors = tsne.csv_to_array(dataset_vec)
-        print len(dataset_vectors)
-        tsne.make_tsne(dataset_2D ,dataset_vectors) 
+        #dataset_vectors = tsne.csv_to_array(dataset_vec)
+#print len(dataset_vectors)
+        tsne.density_tsne(dataset_2D ,dataset_vec) 
 
 if "PS"==str:
     
     print "Loading 3gram vector"
     model_3gram = "./trained_models/ngram_model"
-    model = biovec.models.load_protvec(model_3gram)
+    model = word2vec.models.load_protvec(model_3gram)
     print "... Ok\n"
 
     print "Making tsne"
@@ -49,37 +50,47 @@ elif "BSVM"==str:
         tsne.make_tsne(dataset_2D ,dataset_vectors) 
 
 elif "DM"==str:
-
+    print ("Dis-FGNUP TSNE")
     # Dis-FGNUP
     dis_fg_nups_2D  = "./trained_models/density_map/dis-fg-nups/dis-fg-nups-2D" 
-    dis_fg_nups_vec = "./trained_models/density_map/dis-fg-nups/dis-fg-nups-protein.csv" 
-    protein_tsne(dis_fg_nups_2D , dis_fg_nups_vec)
+    dis_fg_nups_vec = "./trained_models/density_map/dis-fg-nups/dis-fg-nups-ngram-model" 
+    model = word2vec.models.load_protvec(dis_fg_nups_vec)
+    protein_tsne(dis_fg_nups_2D , model)
 
+    print ("FGNUP TSNE")
     # FGNUPS
     fg_nups_2D  = "./trained_models/density_map/fg-nups/fg-nups-2D" 
-    fg_nups_vec = "./trained_models/density_map/fg-nups/fg-nups-protein.csv" 
-    protein_tsne(fg_nups_2D , fg_nups_vec)
+    fg_nups_vec = "./trained_models/density_map/fg-nups/fg-nups-ngram-model" 
+    model = word2vec.models.load_protvec(fg_nups_vec)
+    protein_tsne(fg_nups_2D , model)
 
-
+    print ("PDB1 TSNE")
     # PDB random1
     pdb1_2D  = "./trained_models/density_map/pdb1/pdb1-2D" 
-    pdb1_vec = "./trained_models/density_map/pdb1/pdb1-protein.csv" 
-    protein_tsne(pdb1_2D , pdb1_vec)
+    pdb1_vec = "./trained_models/density_map/pdb1/pdb1-ngram-model" 
+    model = word2vec.models.load_protvec(pdb1_vec)
+    protein_tsne(pdb1_2D , model)
 
+    print ("PDB2 TSNE")
     # PDB random2
     pdb2_2D  = "./trained_models/density_map/pdb2/pdb2-2D" 
-    pdb2_vec = "./trained_models/density_map/pdb2/pdb2-protein.csv" 
-    protein_tsne(pdb2_2D , pdb2_vec)
+    pdb2_vec = "./trained_models/density_map/pdb2/pdb2-ngram-model" 
+    model = word2vec.models.load_protvec(pdb2_vec)
+    protein_tsne(pdb2_2D , model)
 
+    print ("Dis-Disprot TSNE")
     # Dis-Disprot
     dis_disprot_2D  = "./trained_models/density_map/dis-disprot/dis-disprot-2D" 
-    dis_disprot_vec = "./trained_models/density_map/dis-disprot/dis-disprot-protein.csv" 
-    protein_tsne(dis_disprot_2D , dis_disprot_vec)
+    dis_disprot_vec = "./trained_models/density_map/dis-disprot/dis-disprot-ngram-model" 
+    model = word2vec.models.load_protvec(dis_disprot_vec)
+    protein_tsne(dis_disprot_2D , model)
 
+    print ("Disprot TSNE")
     # Disprot
     disprot_2D  = "./trained_models/density_map/disprot/disprot-2D" 
-    disprot_vec = "./trained_models/density_map/disprot/disprot-protein.csv" 
-    protein_tsne(disprot_2D , disprot_vec)
+    disprot_vec = "./trained_models/density_map/disprot/disprot-ngram-model" 
+    model = word2vec.models.load_protvec(disprot_vec)
+    protein_tsne(disprot_2D , model)
 
     visual = bv.BioVisual()
     visual.visual_vec(dis_disprot_2D , disprot_2D ,dis_fg_nups_2D , fg_nups_2D , pdb1_2D , pdb2_2D)
