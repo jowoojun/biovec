31385ed7f9b049ed7be30cabe47d862eb7be9d27
ChanggunLee <cd8114@gmail.com>
2018-03-15 20:38:54 +0900

    pdb process

--- processed_data/processed_sequence.py
+++ processed_data/processed_sequence.py
@@ -14,14 +14,6 @@ f.close()
 
 if not os.path.exists("./binary_svm/dataset.fasta"):
     dataset = open("./binary_svm/dataset.fasta" , "w" )
-    
-    # # parsing FG-NUPS json data
-    # for FG_NUPS in FG_NUPS_data:
-    #     sequence = FG_NUPS["sequence"]
-    #     if 87<= len(sequence):
-    #         name = "FG_NUPS"
-    #         data = '>%s\n%s\n'%(name,sequence)
-    #         dataset.write(data) 
 
     with open ("fg-nups.fasta","r") as f:
         data_list = []
@@ -29,24 +21,29 @@ if not os.path.exists("./binary_svm/dataset.fasta"):
             data_list.append(r.seq)
 
         data_list = np.array(data_list)
+        fg_len = len(data_list)
         for seq in data_list:
             name = "fg-nups"
             data = ">%s\n%s\n" %(name,seq)
             dataset.write(data)
     f.close()
         
-    f = open("pdb_seqres.fasta" , "r")
+    with open("pdb_seqres.fasta" , "r") as f:
+        data_list = []
+        for r in SeqIO.parse(f, "fasta"):
+            data_list.append(r.seq)
 
-    i = 0
-    for line in f:
-        if i%2 == 1 :
-            seq=line
+        data_list = np.array(data_list)
+        data_seq = []
+        for seq in data_list:
             if 890 <= len(seq) and len(seq) <= 910 :
-                name = 'pdb'
-                data = ">%s\n%s"%(name , seq)
+                data_seq.append(seq)
+
+        data_seq = np.random.choice(np.array(data_seq) , fg_len)
+        for seq in data_seq:
+                name = "pdb"
+                data = ">%s\n%s\n" %(name,seq)
                 dataset.write(data)
-                
-        i+=1
     f.close()
     dataset.close()
 
