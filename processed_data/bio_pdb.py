123a9958071390175afc834ea1146eddebb99dbd
ChanggunLee <cd8114@gmail.com>
2018-03-29 23:25:39 +0900

    delete file

--- processed_data/bio_pdb.py
+++
@@ -1,6 +0,0 @@
-from Bio.PDB.PDBParser import PDBParser
-from Bio.PDB.Polypeptide import PPBuilder
-structure = PDBParser().get_structure('2BEG', 'PDB/2BEG.pdb')
-ppb=PPBuilder()
-for pp in ppb.build_peptides(structure):
-    print(pp.get_sequence())
